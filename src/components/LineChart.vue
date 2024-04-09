c6736f30a06bc2beb77ca0b1f966ef3006b9c3ef
CaldatoLuca <lucacaldato15@gmail.com>
2024-04-09 18:08:01 +0200

    feat: line chart

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -14,7 +14,6 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "vue-chartjs";
-import * as chartConfig from "../charts/lineChart";
 
 ChartJS.register(
   CategoryScale,
@@ -32,7 +31,30 @@ export default {
     Line,
   },
   data() {
-    return chartConfig;
+    return {
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [40, 39, 10, 40, 39, 80, 40],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
   },
 };
 </script>
