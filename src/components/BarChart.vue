031e3ead50198d0ee33707492f1448e3bf33ffaf
CaldatoLuca <lucacaldato15@gmail.com>
2024-04-10 11:38:38 +0200

    feat: bar chart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { Bar } from "vue-chartjs";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default {
+  name: "App",
+  components: {
+    Bar,
+  },
+  props: ["x", "y", "name"],
+  data() {
+    return {
+      data: {
+        labels: this.x,
+        datasets: [
+          {
+            label: "Range",
+            backgroundColor: [
+              "#41B883",
+              "#E46651",
+              "#00D8FF",
+              "#DD1B16",
+              "#430A5D",
+            ],
+            data: this.y,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="card col-12 col-md-5">
+    <div class="card-header text-center p-3">{{ this.name }}</div>
+    <div class="card-body">
+      <Bar :data="data" :options="options" />
+    </div>
+  </div>
+</template>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,7 @@
 import Header from "../components/Header.vue";
 import LineChart from "./LineChart.vue";
 import DoughnutChart from "./DoughnutChart.vue";
+import BarChart from "./BarChart.vue";
 import axios from "axios";
 import { store } from "../store";
 
@@ -18,16 +19,22 @@ export default {
         devices: [],
         percentage: [],
       },
+      userAgeRange: {
+        xAxe: [],
+        yAxe: [],
+      },
     };
   },
   components: {
     Header,
     LineChart,
     DoughnutChart,
+    BarChart,
   },
   async mounted() {
     await this.fetchMonthlyConnections(); // Wait for API call to complete
     await this.fetchOperatingSystem();
+    await this.fetchUserAgeRange();
     this.loader = true; // Set loader to true after API call is completed
   },
   methods: {
@@ -55,6 +62,19 @@ export default {
         console.log("Error in fetchOperatingSystem: " + error);
       }
     },
+    async fetchUserAgeRange() {
+      try {
+        const response = await axios.get(
+          store.api.baseUrl + store.api.userAgeRange
+        );
+        const data = response.data;
+
+        this.userAgeRange.xAxe = data.map((item) => item.range);
+        this.userAgeRange.yAxe = data.map((item) => item.connections);
+      } catch (error) {
+        console.log("Error in fetchUserAgeRange: " + error);
+      }
+    },
   },
 };
 </script>
@@ -74,7 +94,11 @@ export default {
       />
 
       <div class="d-flex mb-5 justify-content-between">
-        <DoughnutChart></DoughnutChart>
+        <BarChart
+          :x="this.userAgeRange.xAxe"
+          :y="this.userAgeRange.yAxe"
+          :name="'Users Age Range'"
+        ></BarChart>
         <DoughnutChart
           :devices="this.operatingSystem.devices"
           :percentage="this.operatingSystem.percentage"
